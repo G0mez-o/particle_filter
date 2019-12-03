c89a065aa17c903e3fd217c4cc54f91b74990b33
ShunjiroOsada <shunjiroosada@gmail.com>
2019-12-03 15:55:20 +0900

    modified some files

--- particle_filter.cpp
+++ particle_filter.cpp
@@ -1,5 +1,13 @@
 #include <particle_filter.hpp>
+#include <boost/math/constants/constants.hpp>
 
+const int PARTICLE_NUM = 350;
+#define H_MAX 120
+#define H_MIN 85
+#define S_MAX 255
+#define S_MIN 50
+#define V_MAX 255
+#define V_MIN 50
 int main(int argh, char* argv[])
 {
 
@@ -12,7 +20,7 @@ int main(int argh, char* argv[])
       return -1;
   }
   std::vector<particle> particles;
-  for (int i = 0; i < 100; i++)
+  for (int i = 0; i < PARTICLE_NUM; i++)
   {
     particle buffer_point;
     buffer_point.x = std::rand() % 640;
@@ -35,44 +43,56 @@ int main(int argh, char* argv[])
     // std::cout << (int)buffer[0] << std::endl;
     // std::cout << (int)buffer[1] << std::endl;
     // std::cout << (int)buffer[2] << std::endl;
-    // int center_x = particles[std::rand() % 100].x;
-    // int center_y = particles[std::rand() % 100].y;
-    int center_x;
-    int center_y;
+    // int center_x = particles[std::rand() % PARTICLE_NUM].x;
+    // int center_y = particles[std::rand() % PARTICLE_NUM].y;
+    double center_x;
+    double center_y;
     double likehood_sum = 0.0;
     double buffer_parameter = 0.0;
     double max = 0.0;
-    for (int i = 0; i < 100; i++)
+    cv::Mat hsv_frame;
+    cvtColor(frame, hsv_frame, cv::COLOR_BGR2HSV);
+    cv::Mat mask;
+    cv::Scalar s_min = cv::Scalar(H_MIN, S_MIN, V_MIN);
+    cv::Scalar s_max = cv::Scalar(H_MAX, S_MAX, V_MAX);
+    cv::inRange(hsv_frame, s_min, s_max, mask);
+    for (int i = 0; i < PARTICLE_NUM; i++)
     {
       cv::Vec3b color_buffer = frame.at<cv::Vec3b>(particles[i].y, particles[i].x);
-      float color_dist = std::sqrt((255 -  color_buffer[0]) * (255 - color_buffer[0]) + (color_buffer[1]) * (color_buffer[1])
+      float color_dist = std::sqrt((color_buffer[0]) * (color_buffer[0]) + (color_buffer[1]) * (color_buffer[1])
                                    + (255 - color_buffer[2]) * (255 - color_buffer[2]));
-      particles[i].likehood = std::exp(- color_dist * color_dist / 10000);
+      particles[i].likehood = std::exp(- color_dist * color_dist / 1800) / std::sqrt(3140);
+      // cv::Vec3b color_buffer = mask.at<cv::Vec3b>(particles[i].y, particles[i].x);
+      // float color_dist = (color_buffer[0] + color_buffer[1] + color_buffer[2]) / 3;
+      // float color_dist = std::sqrt((255 - color_buffer[0]) * (255 - color_buffer[0]) + (255 - color_buffer[1]) * (255 - color_buffer[1])
+      //                              + (255 - color_buffer[2]) * (255 - color_buffer[2]));
+      // particles[i].likehood = std::exp(- color_dist * color_dist / 1000);
+
     }
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < PARTICLE_NUM; i++)
     {
       likehood_sum += particles[i].likehood;
     }
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < PARTICLE_NUM; i++)
     {
       particles[i].likehood /= likehood_sum;
     }
-    for (int i = 0; i < 100; i++)
-    {
-      if (max < particles[i].likehood)
-        max = particles[i].likehood;
-      buffer_parameter += particles[i].likehood;
-    }
-    std::cout << max << std::endl;
-    std::cout << buffer_parameter << std::endl;
+    // for (int i = 0; i < PARTICLE_NUM; i++)
+    // {
+    //   if (max < particles[i].likehood)
+    //     max = particles[i].likehood;
+    //   buffer_parameter += particles[i].likehood;
+    // }
+    // std::cout << max << std::endl;
+    // std::cout << buffer_parameter << std::endl;
     // std::cout << (double)std::rand() / RAND_MAX << std::endl;
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < PARTICLE_NUM; i++)
     {
       double sum = 0.0;
       double sum_2 = 0.0;
       int index;
       float rand = (double)std::rand() / RAND_MAX;
-      for (int j = 0; j < 100; j++)
+      for (int j = 0; j < PARTICLE_NUM; j++)
       {
         for (int k = 0; k < j+1; k++)
         {
@@ -81,17 +101,32 @@ int main(int argh, char* argv[])
         }
         sum_2 += particles[j+1].likehood;
         if (sum < rand && sum_2 > rand)
+        {
           index = j + 1;
+          break;
+        }
+          // index = j + 1;
       }
       particles[i].x = particles[index].x;
       particles[i].y = particles[index].y;
     }
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < PARTICLE_NUM; i++)
     {
+      std::random_device seed_gen;
+      std::default_random_engine engine(seed_gen());
+      // 平均0.0、標準偏差1.0で分布させる
+      std::normal_distribution<> dist(0, 15.0);
+      double rand = dist(engine);
+      double rand2 = dist(engine);
+      // std::cout << rand << std::endl;
+      // std::cout << rand2 << std::endl;
+      // double buffer_x = (particles[i].x * particles[i].likehood);
+      // std::cout << buffer_x << std::endl;
       center_x += particles[i].x;
-      particles[i].x += (std::rand() % 21 - 10);
+      particles[i].x += rand;
+      // double buffer_y = (particles[i].y * particles[i].likehood);
       center_y += particles[i].y;
-      particles[i].y += (std::rand() % 21 - 10);
+      particles[i].y += rand2;
       if (particles[i].x > 640)
         particles[i].x = 640;
       if (particles[i].x < 0)
@@ -100,17 +135,27 @@ int main(int argh, char* argv[])
         particles[i].y = 480;
       if (particles[i].y < 0)
         particles[i].y = 0;
-      cv::circle(frame, cv::Point(particles[i].x, particles[i].y), 1, cv::Scalar(0,0,255), -1, CV_AA);
+      cv::circle(frame, cv::Point(particles[i].x, particles[i].y), 1, cv::Scalar(0,255,0), -1, CV_AA);
     }
-    center_x /= 100;
-    center_y /= 100;
+    center_x /= PARTICLE_NUM;
+    center_y /= PARTICLE_NUM;
+    cv::circle(frame, cv::Point((int)center_x, (int)center_y), 10, cv::Scalar(255,0,0), -1, CV_AA);
     std::cout << center_x << std::endl;
     std::cout << center_y << std::endl;
+    // cv::Mat mask;
+    // cv::Scalar s_min = cv::Scalar(H_MIN, S_MIN, V_MIN);
+    // cv::Scalar s_max = cv::Scalar(H_MAX, S_MAX, V_MAX);
+    // cv::inRange(hsv_frame, s_min, s_max, mask);
+    // cv::Vec3b buffer = mask.at<cv::Vec3b>(0, 0);
+    // std::cout << buffer << std::endl;
     cv::imshow("win", frame);//画像を表示
     // cv::Mat hsv_frame;
     // cvtColor(frame, hsv_frame, cv::COLOR_BGR2HSV);
     // cv::imshow("hsv", hsv_frame);
-    cv::moveWindow("win", 400, 400);
+    cv::imshow("mask", mask);
+    cv::moveWindow("win", 200, 200);
+    // cv::moveWindow("hsv", 600, 600);
+    cv::moveWindow("mask", 1000, 200);
     const int key = cv::waitKey(1);
     if(key == 'q'/*113*/)//qボタンが押されたとき
     {
